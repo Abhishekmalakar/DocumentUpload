79342d49338f568a3dbf93c208734972f288c6b3
Abhishek Malakar <malakarabhishek1998@gmail.com>
2023-06-02 08:09:18 +0530

    Document upload

---
+++ Document_Digitization_App/src/app/app.component.spec.ts
@@ -0,0 +1,27 @@
+import { TestBed } from '@angular/core/testing';
+import { AppComponent } from './app.component';
+
+describe('AppComponent', () => {
+  beforeEach(() => TestBed.configureTestingModule({
+    declarations: [AppComponent]
+  }));
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it(`should have as title 'Document_Digitization_App'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('Document_Digitization_App');
+  });
+
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Document_Digitization_App app is running!');
+  });
+});
---
+++ Document_Digitization_App/src/src.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@angular/core';
+
+
+export const SrcService= {
+  production: false,
+  apiUrl: 'https://localhost:5001/api',
+  constructor() { }
+}
+
